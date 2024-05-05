71efa91a69227a96fde9c236992632063e42664d
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-05-05 19:21:24 -0400

    added config.py file

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ## Description
 
+Designed with guidance from https://github.com/bajcmartinez/flask-api-starter-kit/blob/master/app.py
--- api/app.py
+++ api/app.py
@@ -1,1 +1,28 @@
-import requests
+from flask import Flask
+from flasgger import Swagger
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SWAGGER'] = {
+        'title': 'Stock Statistics API',
+    }
+    swagger = Swagger(app)
+     ## Initialize Config
+    # app.config.from_pyfile('config.py')
+    # app.register_blueprint(home_api, url_prefix='/api')
+
+    return app
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen to, not starboard')
+    args = parser.parse_args()
+    port = args.port
+
+    app = create_app()
+
+    app.run(port=port)
