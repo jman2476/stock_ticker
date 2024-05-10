2f2f18fd2501cbbad841391d6baa07939cacf3b2
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-05-10 11:34:23 -0400

    wrote analysis functions and sqlite queries, still wip

--- api/schema/quotes.py
+++ api/schema/quotes.py
@@ -5,7 +5,7 @@ from marshmallow.fields import Str
 class QuotesSchema(Schema):
     class Meta: 
         # Fields to expose
-        fields = ["TW_quote","Finn_quote"]
+        fields = ["TW_quote","Finn_quote", "Yfinance"]
 
     TW_quote = Str()
     Finn_quote = Str()
